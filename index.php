f82edda158bcb09c1161c7ab1770149141aedb90
Enrique Hidalgo <koko.freelance@gmail.com>
2021-10-27 20:22:20 +0100

    basic form added

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login PHP Project</title>
+  <link rel="stylesheet" href="./css/index.css">
+</head>
+
+<body>
+
+  <main>
+    <div class="form">
+      <form action="" class="form__content">
+        <label for="user" class="form__label">User Name</label>
+        <input type="text" class="form__input">
+        <label for="password" class="form__label">Password</label>
+        <input type="password" class="form__input">
+        <button type="submit" class="form__button">Log In</button>
+      </form>
+    </div>
+
+
+  </main>
+
+</body>
+
+</html>
